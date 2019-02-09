08081c8b439ede8675aebd34d580604d955e34bc
Ushakov Michael <um.nix.user@gmail.com>
2019-02-10 00:37:09 +0400

    project created

---
+++ RomReader/src/rom_reader.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:36:08 02/10/2019 
+// Design Name: 
+// Module Name:    rom_reader 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module rom_reader(
+    );
+
+
+endmodule
