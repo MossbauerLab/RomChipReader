34d0307cc2a39c04a45d09d55d8ebde5ad177125
Ushakov Michael <um.nix.user@gmail.com>
2019-05-18 23:06:40 +0300

    7seg encoder function is ready

--- RomReader/src/address_display.v
+++ RomReader/src/address_display.v
@@ -73,9 +73,20 @@ function [11:0] encode_to_bcd;
 	 encode_to_bcd[11:8] = hundreds[3:0];
 endfunction
 
-function [7:0] encode_to_sseg
+function [6:0] encode_to_sseg
 input [3:0] bcd
-    return 0;
+    case (bcd)
+	 4'b0000: encode_to_sseg = 7'b1111110;
+	 4'b0001: encode_to_sseg = 7'b0110000;
+	 4'b0010: encode_to_sseg = 7'b1101101;
+	 4'b0011: encode_to_sseg = 7'b1111001;
+	 4'b0100: encode_to_sseg = 7'b0110011;
+	 4'b0101: encode_to_sseg = 7'b1011011;
+	 4'b0110: encode_to_sseg = 7'b1011111;
+	 4'b0111: encode_to_sseg = 7'b1110000;
+	 4'b1000: encode_to_sseg = 7'b1111111;
+	 4'b1001: encode_to_sseg = 7'b1111011;
+	 endcase
 endfunction
 
 
